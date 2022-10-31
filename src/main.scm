ce73e14b7b9720982b28760f2e9964eb60660af5
Jason Walsh <jwalsh1@chewy.com>
2022-10-31 09:49:58 -0400

    demo prep

--- src/main.scm
+++ src/main.scm
@@ -3,11 +3,11 @@
 ;; 1 Quick Start
 (struct pt ([x : Real] [y : Real]))
 
-(: distance (-> pt pt Real))
-(define (distance p1 p2)
+(: jwalsh/distance (-> pt pt Real))
+(define (jwalsh/distance p1 p2)
   (sqrt (+ (sqr (- (pt-x p2) (pt-x p1))) (sqr (- (pt-y p2) (pt-y p1))))))
 
-(quote (distance (pt 0 0) (pt 3.1415 2.7172)))
+(quote (jwalsh/distance (pt 0 0) (pt 3.1415 2.7172)))
 
 ;; Trees and Recursion
 
@@ -16,105 +16,105 @@
 (struct node ([left : Tree] [right : Tree]))
 (define-type Tree (U leaf node))
 
-(: tree-height (-> Tree Integer))
-(define (tree-height t)
+(: jwalsh/tree-height (-> Tree Integer))
+(define (jwalsh/tree-height t)
   (cond
     [(leaf? t) 1]
-    [else (max (+ 1 (tree-height (node-left t))) (+ 1 (tree-height (node-right t))))]))
+    [else (max (+ 1 (jwalsh/tree-height (node-left t))) (+ 1 (jwalsh/tree-height (node-right t))))]))
 
-(: tree-sum (-> Tree Number))
-(define (tree-sum t)
+(: jwalsh/tree-sum (-> Tree Number))
+(define (jwalsh/tree-sum t)
   (cond
     [(leaf? t) (leaf-val t)]
-    [else (+ (tree-sum (node-left t)) (tree-sum (node-right t)))]))
+    [else (+ (jwalsh/tree-sum (node-left t)) (jwalsh/tree-sum (node-right t)))]))
 
-(: tree-map (-> (-> Number Number) Tree Tree))
-(define (tree-map f t)
+(: jwalsh/tree-map (-> (-> Number Number) Tree Tree))
+(define (jwalsh/tree-map f t)
   (cond
     [(leaf? t) (leaf (f (leaf-val t)))]
-    [else (node (tree-map f (node-left t)) (tree-map f (node-right t)))]))
+    [else (node (jwalsh/tree-map f (node-left t)) (jwalsh/tree-map f (node-right t)))]))
 
 (quote (define my-tree (node (leaf 1) (node (leaf 2) (leaf 3))))
-       (tree-height my-tree)
-       (tree-sum my-tree)
-       (tree-map (lambda (x) (* x x)) my-tree))
+       (jwalsh/tree-height my-tree)
+       (jwalsh/tree-sum my-tree)
+       (jwalsh/tree-map (lambda (x) (* x x)) my-tree))
 
 ;; RacketCon 2022 samples
-(: middle (-> (Listof Number) Number))
-(define (middle l1)
+(: jwalsh/middle (-> (Listof Number) Number))
+(define (jwalsh/middle l1)
   (define n (length l1))
   (list-ref l1 (quotient n 2)))
 
-(: mean (-> (Listof Number) Number))
-(define (mean l1)
+(: jwalsh/mean (-> (Listof Number) Number))
+(define (jwalsh/mean l1)
   (define n (length l1))
   (define sum (foldl + 0 l1))
   (/ sum n))
 
-(: median (-> (Listof Number) Number))
-(define (median l1)
+(: jwalsh/median (-> (Listof Number) Number))
+(define (jwalsh/median l1)
   (define l2 (sort < l1))
   (middle l2))
 
-(: mode (-> (Listof Number) Number))
-(define (mode l1)
+(: jwalsh/mode (-> (Listof Number) Number))
+(define (jwalsh/mode l1)
   (define l2 (sort < l1))
   (define l3 (group-by (lambda (x y) (= x y)) l2))
   (define l4 (map (lambda (x) (cons (length x) (car x))) l3))
   (define l5 (sort (lambda (x y) (> (car x) (car y))) l4))
   (cdr (car l5)))
 
-(: range (-> (Listof Number) Number))
-(define (range l1)
+(: jwalsh/range (-> (Listof Number) Number))
+(define (jwalsh/range l1)
   (define l2 (sort < l1))
   (- (car (last l2)) (car l2)))
 
-(: variance (-> (Listof Number) Number))
-(define (variance l1)
+(: jwalsh/variance (-> (Listof Number) Number))
+(define (jwalsh/variance l1)
   (define n (length l1))
   (define m (mean l1))
   (define l2 (map (lambda (x) (- x m)) l1))
   (define l3 (map (lambda (x) (* x x)) l2))
   (define sum (foldl + 0 l3))
   (/ sum n))
-n
 
-(: standard-deviation (-> (Listof Number) Number))
-(define (standard-deviation l1)
+
+(: jwalsh/standard-deviation (-> (Listof Number) Number))
+(define (jwalsh/standard-deviation l1)
   (sqrt (variance l1)))
 
-(: z-score (-> (Listof Number) Number Number))
-(define (z-score l1 x)
-  (define m (mean l1))
-  (define s (standard-deviation l1))
+(: jwalsh/z-score (-> (Listof Number) Number Number))
+(define (jwalsh/z-score l1 x)
+  (define m (jwalsh/mean l1))
+  (define s (jwalsh/standard-deviation l1))
   (/ (- x m) s))
 
 (quote (define my-list (list 3 2 1 1))
-       (middle my-list) ;; 2
-       (mean my-list) ;; 1.75
-       (median my-list) ;; 1.5
-       (mode my-list) ;; 1
-       (range my-list) ;; 2
+       (jwalsh/middle my-list) ;; 2
+       (jwalsh/mean my-list) ;; 1.75
+       (jwalsh/median my-list) ;; 1.5
+       (jwalsh/mode my-list) ;; 1
+       (jwalsh/range my-list) ;; 2
        )
 
 ;; Sorting
-(: quicksort (-> (Listof Number) (Listof Number)))
-(define (quicksort l1)
+(: jwalsh/quicksort (-> (Listof Number) (Listof Number)))
+(define (jwalsh/quicksort l1)
   (cond
     [(null? l1) '()]
     [else
      (let ([pivot (car l1)] [rest (cdr l1)])
-       (append (quicksort (filter (lambda (x) (< x pivot)) rest))
-               (cons pivot (quicksort (filter (lambda (x) (>= x pivot)) rest)))))]))
+       (append (jwalsh/quicksort (filter (lambda (x) (< x pivot)) rest))
+               (cons pivot (jwalsh/quicksort (filter (lambda (x) (>= x pivot)) rest)))))]))
 
-(: mergesort (-> (Listof Number) (Listof Number)))
-(define (mergesort l1)
+(: jwalsh/mergesort (-> (Listof Number) (Listof Number)))
+(define (jwalsh/mergesort l1)
   (cond
     [(null? l1) '()]
     [(null? (cdr l1)) l1]
     [else
      (let ([l2 (split-at (quotient (length l1) 2) l1)])
-       (merge (mergesort (car l2)) (mergesort (cdr l2))))]))
+       (merge (jwalsh/mergesort (car l2)) (jwalsh/mergesort (cdr l2))))]))
 
 ; Examples
 ;; review contracts and transitivity requirements for sort
@@ -122,77 +122,77 @@ n
        (define my-bad-sort-predicate2 (lambda (x y) #f)))
 
 ;; Lists and Recursion
-(: sum (-> (Listof Number) Number))
-(define (sum l1)
+(: jwalsh/sum (-> (Listof Number) Number))
+(define (jwalsh/sum l1)
   (cond
     [(null? l1) 0]
     [else (+ (car l1) (sum (cdr l1)))]))
 
-(: product (-> (Listof Number) Number))
-(define (product l1)
+(: jwalsh/product (-> (Listof Number) Number))
+(define (jwalsh/product l1)
   (cond
     [(null? l1) 1]
     [else (* (car l1) (product (cdr l1)))]))
 
-(: length (-> (Listof Number) Integer))
-(define (length l1)
+(: jwalsh/length (-> (Listof Number) Integer))
+(define (jwalsh/length l1)
   (cond
     [(null? l1) 0]
     [else (+ 1 (length (cdr l1)))]))
 
-(: append (-> (Listof Number) (Listof Number) (Listof Number)))
-(define (append l1 l2)
+(: jwalsh/append (-> (Listof Number) (Listof Number) (Listof Number)))
+(define (jwalsh/append l1 l2)
   ) ;; TODO
 
-(: reverse (-> (Listof Number) (Listof Number))) ;; TODO
-(define (reverse l1)
+(: jwalsh/reverse (-> (Listof Number) (Listof Number))) ;; TODO
+(define (jwalsh/reverse l1)
   )
 
-(: sort (-> (Listof Number) (Listof Number))) ;; TODO
-(define (sort l1)
+(: jwalsh/sort (-> (Listof Number) (Listof Number))) ;; TODO
+(define (jwalsh/sort l1)
   )
 
-(: filter (-> (-> Number Boolean) (Listof Number) (Listof Number))) ;; TODO
-(define (filter f l1)
+(: jwalsh/filter (-> (-> Number Boolean) (Listof Number) (Listof Number))) ;; TODO
+(define (jwalsh/filter f l1)
   )
 
-(: map (-> (-> Number Number) (Listof Number) (Listof Number))) ;; TODO
-(define (map f l1)
+(: jwalsh/map (-> (-> Number Number) (Listof Number) (Listof Number))) ;; TODO
+(define (jwalsh/map f l1)
   )
 
-(: foldl (-> (-> Number Number Number) Number (Listof Number) Number)) ;; TODO
-(define (foldl f init l1)
+(: jwalsh/foldl (-> (-> Number Number Number) Number (Listof Number) Number)) ;; TODO
+(define (jwalsh/foldl f init l1)
   )
 
-(: foldr (-> (-> Number Number Number) Number (Listof Number) Number)) ;; TODO
-(define (foldr f init l1)
+(: jwalsh/foldr (-> (-> Number Number Number) Number (Listof Number) Number)) ;; TODO
+(define (jwalsh/foldr f init l1)
   )
 
-(: zip (-> (Listof Number) (Listof Number) (Listof (Listof Number)))) ;; TODO
-(define (zip l1 l2)
+(: jwalsh/zip (-> (Listof Number) (Listof Number) (Listof (Listof Number)))) ;; TODO
+(define (jwalsh/zip l1 l2)
   )
 
-(: unzip (-> (Listof (Listof Number)) (Listof Number) (Listof Number))) ;; TODO
-(define (unzip l1)
+(: jwalsh/unzip (-> (Listof (Listof Number)) (Listof Number) (Listof Number))) ;; TODO
+(define (jwalsh/unzip l1)
   )
 
-(: zip-with (-> (-> Number Number Number) (Listof Number) (Listof Number) (Listof Number))) ;; TODO
-(define (zip-with f l1 l2)
+(: jwalsh/zip-with (-> (-> Number Number Number) (Listof Number) (Listof Number) (Listof Number))) ;; TODO
+(define (jwalsh/zip-with f l1 l2)
   )
 
-(: zip-with* (-> (-> Number Number Number) (Listof Number) (Listof Number) (Listof Number))) ;; TODO
-(define (zip-with* f l1 l2)
+(: jwalsh/zip-with* (-> (-> Number Number Number) (Listof Number) (Listof Number) (Listof Number))) ;; TODO
+(define (jwalsh/zip-with* f l1 l2)
   )
 
-(: flatten (-> (Listof (Listof Number)) (Listof Number)))
-(define (flatten l1)
+(: jwalsh/flatten (-> (Listof (Listof Number)) (Listof Number)))
+(define (jwalsh/flatten l1)
   (cond
     [(null? l1) '()]
     [(list? (car l1)) (append (flatten (car l1)) (flatten (cdr l1)))]
     [else (cons (car l1) (flatten (cdr l1)))]))
 
-(: flatten* (-> (Listof (Listof Number)) (Listof Number)))
-(define (flatten* l1)
+(: jwalsh/flatten* (-> (Listof (Listof Number)) (Listof Number)))
+(define (jwalsh/flatten* l1)
   (cond
     [(null? l1) '()]
     [(list? (car l1)) (append (flatten* (car l1)) (flatten* (cdr l1)))]
@@ -219,8 +219,8 @@ n
 
 ;; Transformations and Recursion
 
-(: frequencies (-> (Listof Number) (Listof (Listof Number))))
-(define (frequencies l1)
+(: jwalsh/frequencies (-> (Listof Number) (Listof (Listof Number))))
+(define (jwalsh/frequencies l1)
   (cond
     [(null? l1) '()]
     [else
@@ -228,8 +228,8 @@ n
        (cons (cons x (length (filter (lambda (y) (= x y)) rest)))
              (frequencies (filter (lambda (y) (not (= x y))) rest))))]))
 
-(: permutations (-> (Listof Number) (Listof (Listof Number))))
-(define (permutations l1)
+(: jwalsh/permutations (-> (Listof Number) (Listof (Listof Number))))
+(define (jwalsh/permutations l1)
   (cond
     [(null? l1) '()]
     [else
@@ -242,13 +242,13 @@ n
        (permutations my-list4) ;;
        )
 
-(define my-friends (list '("John" "Mary" "Peter" "Jane" "Bob")))
+(define jwalsh/friends (list '("John" "Mary" "Peter" "Jane" "Bob")))
 
-(define my-pets (list (list 'dog 'fido) (list 'cat 'fluffy) (list 'fish 'nemo)))
+(define jwalsh/pets (list (list 'dog 'fido) (list 'cat 'fluffy) (list 'fish 'nemo)))
 
 ; (struct pet ([species : Symbol] [name : Symbol]))
 
-(define my-books
+(define jwalsh/books
   (list (list 'title "The Hobbit")
         (list 'author "J.R.R. Tolkien")
         (list 'year 1937)
@@ -261,7 +261,7 @@ n
 
 ; (struct book ([title string] [author string] [year number]))
 
-(define my-people
+(define jwalsh/people
   (list (list 'name "John")
         (list 'age 30)
         (list 'name "Mary")
@@ -273,7 +273,7 @@ n
         (list 'name "Bob")
         (list 'age 35)))
 
-(define my-philosophers
+(define jwalsh/philosophers
   (list (list 'name "Plato")
         (list 'born 428)
         (list 'died 348)
@@ -284,7 +284,7 @@ n
         (list 'born 470)
         (list 'died 399)))
 
-(define (person? x)
+(define (jwalsh/person? x)
   (and (pair? x)
        (eq? (car x) 'person)
        (pair? (cdr x))
@@ -293,16 +293,16 @@ n
        (integer? (cadddr x))
        (integer? (cddddr x))))
 
-(define (person-name x)
+(define (jwalsh/person-name x)
   (cadr x))
 
-(define (person-age x)
+(define (jwalsh/person-age x)
   (caddr x))
 
-(define (person-born x)
+(define (jwalsh/person-born x)
   (cadddr x))
 
-(define (person-died x)
+(define (jwalsh/person-died x)
   (cddddr x))
 
 (struct person ([name : String] [age : Integer] [born : Integer] [died : Integer]))
